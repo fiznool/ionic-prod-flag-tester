1ef6eb61b8fc368d59f860b4b256dc217ab4bc0f
Tom Spencer <tom.spencer@studiozeffa.com>
2017-07-03 21:44:31 +0100

    Added custom webpack config

---
+++ config/webpack.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var path = require('path');
+var merge = require('webpack-merge');
+
+var projectRootDir = process.env.IONIC_ROOT_DIR;
+var appScriptsDir = process.env.IONIC_APP_SCRIPTS_DIR;
+
+var originalConfig = require(path.join(appScriptsDir, 'config', 'webpack.config.js'));
+
+var config = {
+  resolve: {
+    alias: {
+      src: path.join(projectRootDir, 'src')
+    }
+  }
+};
+
+module.exports = merge(originalConfig, config);
